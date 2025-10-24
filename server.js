a51fe1eb447a3bb0ab663a40bf72078926b8c683
samuel-m765 <sammbogo81@gmail.com>
2025-10-24 08:33:58 +0300

    added a user notification message

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js
+
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -10,15 +10,29 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
+
+const users = {};
+
 io.on("connection", (socket) => {
   console.log("A user connected");
 
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg); // send message to all users
+  
+  socket.on("set username", (username) => {
+    users[socket.id] = username;
+    io.emit("user connected", `${username} joined the chat 💬`);
   });
 
+  
+  socket.on("chat message", (msg) => {
+    const username = users[socket.id] || "Anonymous";
+    io.emit("chat message", { user: username, text: msg });
+  });
+
+  
   socket.on("disconnect", () => {
-    console.log("A user disconnected");
+    const username = users[socket.id] || "Someone";
+    io.emit("user disconnected", `${username} left the chat 👋`);
+    delete users[socket.id];
   });
 });
 
